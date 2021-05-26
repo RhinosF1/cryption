eda3a42292a29cf18981eee9f6b767835137f3b8
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-05-26 14:03:39 +0100

    Create test_cryption.py

---
+++ tests/test_cryption.py
@@ -0,0 +1,11 @@
+from pytest import fixture
+
+from epqsource.cryption import decrypthash, encryptstring, make_keys
+
+
+def test_encrypt_decrypt_sequence_using_generate_keys():
+    keys = make_keys()
+    encoded_hash = encryptstring(
+        keys.public, "The quick brown fox jumped over the lazy dog")
+    decryptedtext = decrypthash(keys.private, encoded_hash)
+    assert "The quick brown fox jumped over the lazy dog" == decryptedtext
